a49a9601e607c30ee302f848b0fd6f00b5dabf4b
Putu Agus Putrawan <148840348+agusputrawan@users.noreply.github.com>
2024-10-04 18:38:53 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,11 +6,9 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 
-# # Memasukkan file CSV
+# Memasukkan file CSV
 file_path = "dashboard/main_data.csv"
- day_df = pd.read_csv(file_path)
-
-
+day_df = pd.read_csv(file_path)
 
 # Konversi kolom 'tanggal' menjadi datetime
 day_df['tanggal'] = pd.to_datetime(day_df['tanggal'])
@@ -33,9 +31,9 @@ start_date, end_date = st.sidebar.date_input(
 # Filter data berdasarkan tanggal yang dipilih
 filtered_data = day_df[(day_df['tanggal'] >= pd.to_datetime(start_date)) & (day_df['tanggal'] <= pd.to_datetime(end_date))]
 
-# with st.sidebar:
-     # Menambah logo
-     st.image("dashboard/Bike_rental.jpg")
+with st.sidebar:
+    # Menambah logo
+    st.image("dashboard/Bike_rental.jpg")
 
 # Judul
 st.title("Dashboard Penyewaan Sepeda:sparkles:")
