76b6842b9fe31f894effe97596f941cd5f65bf67
Putu Agus Putrawan <148840348+agusputrawan@users.noreply.github.com>
2024-10-04 17:50:19 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -10,19 +10,25 @@ day_df = pd.read_csv(file_path)
 
 # Konversi kolom 'tanggal' menjadi datetime
 day_df['tanggal'] = pd.to_datetime(day_df['tanggal'])
+datetime_columns = ["tanggal"]
+day_df.sort_values(by="tanggal", inplace=True)
+day_df.reset_index(inplace=True)
 
-# Filter Sidebar= untuk memelih tanggal
+# Filter Sidebar untuk memfilter tanggal
 st.sidebar.header("Filter Data")
 min_date = day_df['tanggal'].min()
 max_date = day_df['tanggal'].max()
-date_filter = st.sidebar.date_input("Pilih Tanggal", min_date)
 
-with st.sidebar:
-    # Menambahkan logo
-    st.image("Bike_rental.jpg")
+# Membuat komponen filter
+start_date, end_date = st.sidebar.date_input(
+    label='Range of Time', 
+    min_value=min_date,
+    max_value=max_date,
+    value=[min_date, max_date]
+)
 
-# Filter data berdasarkan tanggal 
-filtered_data = day_df[day_df['tanggal'] == pd.to_datetime(date_filter)]
+# Filter data berdasarkan tanggal yang dipilih
+filtered_data = day_df[(day_df['tanggal'] >= pd.to_datetime(start_date)) & (day_df['tanggal'] <= pd.to_datetime(end_date))]
 
 # Judul
 st.title("Dashboard Penyewaan Sepeda:sparkles:")
@@ -37,11 +43,27 @@ st.write(f"Total Pengguna Kasual: {total_casual_users}")
 st.write(f"Total Pengguna Terdaftar: {total_registered_users}")
 st.write(f"Total Pengguna: {total_users}")
 
+# User perhari
+st.subheader('Jumlah Pengguna Harian')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    total_casual = filtered_data['user_kasual'].sum()
+    st.metric("Total Casual User", value=f'{total_casual:,}')
+
+with col2:
+    total_registered = filtered_data['user_terdaftar'].sum()
+    st.metric("Total Registered User", value=f'{total_registered:,}')
+
+with col3:
+    total_users = filtered_data['user_total'].sum()
+    st.metric("Total Users", value=f'{total_users:,}')
+
 # Visualisasi Jumlah Pengguna Kasual dan Terdaftar per Hari
 st.header("Jumlah Pengguna Kasual dan Terdaftar per Hari")
 plt.figure(figsize=(10, 6))
-sns.lineplot(data=day_df, x='tanggal', y='user_kasual', label='Pengguna Kasual')
-sns.lineplot(data=day_df, x='tanggal', y='user_terdaftar', label='Pengguna Terdaftar')
+sns.lineplot(data=filtered_data, x='tanggal', y='user_kasual', label='Pengguna Kasual')
+sns.lineplot(data=filtered_data, x='tanggal', y='user_terdaftar', label='Pengguna Terdaftar')
 plt.title('Jumlah Pengguna Kasual dan Terdaftar per Hari')
 plt.xlabel('Tanggal')
 plt.ylabel('Jumlah Pengguna')
@@ -52,17 +74,17 @@ st.pyplot(plt)
 # Visualisasi Suhu dan Jumlah Penyewaan
 st.header("Visualisasi Suhu dan Jumlah Penyewaan")
 plt.figure(figsize=(10, 6))
-sns.scatterplot(data=day_df, x='temperatur', y='user_total')
+sns.scatterplot(data=filtered_data, x='temperatur', y='user_total')
 plt.title('Hubungan antara Suhu dan Jumlah Penyewaan')
 plt.xlabel('Temperatur')
 plt.ylabel('Jumlah Penyewaan')
 st.pyplot(plt)
 
 # Analisa Korelasi
-correlation_temp_rentals = day_df['temperatur'].corr(day_df['user_total'])
+correlation_temp_rentals = filtered_data['temperatur'].corr(filtered_data['user_total'])
 st.write(f"Korelasi antara Temperatur dan Jumlah Penyewaan: {correlation_temp_rentals}")
 
-
+# Hasil Regresi Linear
 st.subheader("Hasil Regresi Linear")
 st.write("""
 - Koefisien Intersep: 1214.64
@@ -74,7 +96,7 @@ st.write("""
 
 # Visualisasi Jumlah Penyewaan per Musim
 st.header("Jumlah Penyewaan Sepeda per Musim")
-seasonal_rentals = day_df.groupby('musim')['user_total'].sum().reset_index()
+seasonal_rentals = filtered_data.groupby('musim')['user_total'].sum().reset_index()
 plt.figure(figsize=(10, 6))
 sns.barplot(data=seasonal_rentals, x='musim', y='user_total')
 plt.title('Jumlah Penyewaan Sepeda per Musim')
@@ -83,7 +105,7 @@ plt.ylabel('Jumlah Penyewaan Sepeda')
 st.pyplot(plt)
 
 # Analisis Varians (ANOVA)
-groups = [group['user_total'].values for name, group in day_df.groupby('musim')]
+groups = [group['user_total'].values for name, group in filtered_data.groupby('musim')]
 f_statistic, p_value = stats.f_oneway(*groups)
 st.write(f'F-statistic: {f_statistic}, p-value: {p_value}')
 
